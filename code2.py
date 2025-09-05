f548406bce0c71a9199d7ee8e2d083126f86fdab
AryanBhansali-SE <aryan.bhansali2004@gmail.com>
2025-09-06 03:25:48 +0530

    add first 3 HackerRank solutions

---
+++ code2.py
@@ -0,0 +1,19 @@
+'''Task
+The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+1. The first line contains the sum of the two numbers.
+2. The second line contains the difference of the two numbers (first - second).
+3. The third line contains the product of the two numbers.
+Example
+a = 3
+b = 5
+'''
+
+if __name__ == '__main__':
+    a = int(input("Enter value 1:"))
+    b = int(input("Enter Value 2:"))
+    
+    
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
